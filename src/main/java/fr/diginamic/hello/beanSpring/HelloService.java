7c5603a01a8fcc72befeda2b4c8a0fea9491e20a
johan guillen <sioupes34@gmail.com>
2024-11-22 13:00:20 +0100

    tp2

--- src/main/java/fr/diginamic/hello/beanSpring/HelloService.java
+++ src/main/java/fr/diginamic/hello/beanSpring/HelloService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class HelloService {
+
+
     public String salutation() {
         return "Je suis la classe de service et je vous dis Bonjour";
     }
